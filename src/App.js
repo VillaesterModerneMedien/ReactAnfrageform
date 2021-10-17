e1374f5d2fa0a4bfb33bf13a417e80869a846035
info@whykiki.de <421bbdc0ad+>
2021-10-17 21:20:49 +0200

    - Steps angepasst

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
+    useLocation,
+    useHistory,
     Link
 } from 'react-router-dom'
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import './App.css';
 import { AppContextProvider } from './components/AppContext';
 import Step from './Steps/Step.js'
@@ -14,37 +17,47 @@ import Components from './components'
 
 function App() {
 
+    const location = useHistory();
+    console.log("location", location);
+
   return (
       <AppContextProvider>
           <Router>
-              <Switch>
-                  <Route exact path="/multistep">
-                      <div className="App">
-                          aaa
-                      </div>
-                  </Route>
-                  <Route exact path="/">
-                      <div className="App">
-                      </div>
-                  </Route>
-                  <Route exact path="/wurst">
-                      <div className="App">
+              <TransitionGroup>
+                  <CSSTransition
+                     // key={location.pathname}
+                      classNames="fade"
+                      timeout={600}
+                  >
+                  <Switch>
+                      <Route exact path="/multistep">
+                          <div className="App">
+                              aaa
+                          </div>
+                      </Route>
+                      <Route exact path="/">
+                          <div className="App">
+                          </div>
+                      </Route>
+                      <Route exact path="/wurst">
+                          <div className="App">
 
-                      </div>
-                  </Route>
-                  <Route
-                      exact
-                      path="/step/:id"
-                      render={props => (
-                          <Step {...props} title={`Props through render`} />
-                      )}
-                  />
-                  <Route path='/formStep/' >
-                      <h1>Hello React</h1>
-                      {jsonData.map(block => Components(block))}
-                  </Route>
-              </Switch>
-
+                          </div>
+                      </Route>
+                      <Route
+                          exact
+                          path="/step/:id"
+                          render={props => (
+                              <Step {...props} title={`Props through render`} />
+                          )}
+                      />
+                      <Route path='/formStep/' >
+                          <h1>Hello React</h1>
+                          {jsonData.map(block => Components(block))}
+                      </Route>
+                  </Switch>
+                  </CSSTransition>
+              </TransitionGroup>
           </Router>
       </AppContextProvider>
   );
--- src/Steps/Step.js
+++ src/Steps/Step.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import './Step.scss'
 import {
     BrowserRouter as Router,
     Link,
@@ -6,43 +7,49 @@ import {
     Routes,
     useParams
 } from "react-router-dom";
+
 import jsonData from '../steps.json';
 import Components from "../components";
 
 function Step({title}){
-
+    const [stepId, setStepId] = useState(0);
     const { id } = useParams();
-    const [ state, setState ] = useState({
-        book: null
-    })
+    const navState = jsonData[stepId]['includeNav']
 
-    console.log(jsonData[id])
+    useEffect(() => {
+       setStepId(parseInt(id) - 1);
+    });
 
     let fields = [];
 
-    if(jsonData[id])
+
+    if(jsonData[stepId])
     {
-        fields = jsonData[id]?.fields
-        console.log(fields)
+        fields = jsonData[stepId]?.fields
+        //console.log(fields)
     }
     else{
         fields = false
     }
 
-
+    //console.log(jsonData[stepId]);
 
     return (
-        <div>
-
+        <div className="stepContainer">
+            <h1>{jsonData[stepId]['name']}</h1>
+            <p>{jsonData[stepId]['question']}</p>
             {(fields) && (
-                console.log('felder',fields),
+                //console.log('felder',fields),
                 fields.map(block => Components(block))
             )}
 
-            <ul className="navButtons">
-                <li>Previous</li>
-                <li>Next</li>
-            </ul>
+            {(navState) && (
+                <ul className="navButtons">
+                    <li>Previous</li>
+                    <li>Next</li>
+                </ul>
+            )}
+
         </div>
     )
 }
